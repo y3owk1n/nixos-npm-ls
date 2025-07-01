eccd340a7b49d85cb55692badb2492f43bc7947b
y3owk1n <62775956+y3owk1n@users.noreply.github.com>
2025-07-01 12:24:39 +0000

    chore: update prisma-language-server

--- servers/prisma-language-server/default.nix
+++ servers/prisma-language-server/default.nix
@@ -6,8 +6,8 @@
 }:
 let
   name = "prisma-language-server";
-  version = "6.10.1";
-  sha256 = "sha256-8Gqcb5pBzUltKfY/zN1nDLg+FpALgpc2G77uEOto09c=";
+  version = "6.11.0";
+  sha256 = "sha256-FBj/NwtOdMCNek4p6wRCQAuPg6nw+8YXExOKlPyTeMk=";
 in
 {
   imports = [
